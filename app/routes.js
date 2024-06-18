9e7bc4b56c8050a5e19bf709c9db697038a745f2
maryharmon79 <mary.harmon@sparck.io>
2024-06-18 17:36:55 +0100

    Adding bulk properties

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,19 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+// Run this code when a form is submitted to 'add-properties-method'
+router.post('/add-properties-method', function (req, res) {
+
+    // Make a variable
+    var whichMethod = req.session.data['addProperties']
+  
+    // Check whether the variable matches a condition
+    if (whichMethod == "individual"){
+      // Send user to next page
+      res.redirect('/hug/properties/new-individual-address')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/hug/properties/new-bulk')
+    }
+  
+  })
